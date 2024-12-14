47a88d809d3c6411d9cf4eba49770e54f658629d
Debarup <87480898+debarup24@users.noreply.github.com>
2024-12-14 17:37:39 +0530

    Travel website

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,83 @@
-import "./App.css";
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Pages/Home";
+import Tours from "./Pages/Tours";
+import Gallery from "./Pages/Gallery";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Footer from "./Components/Footer";
+import ScrollToTop from "react-scroll-to-top";
 
-function App() {
-  return <div>Travel app</div>;
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Navbar />
+        <Home />
+        <Footer />
+      </>
+    ),
+  },
+  // {
+  //   path: "/tours",
+  //   element: (
+  //     <>
+  //       <Navbar />
+  //       <Tours />
+  //       <Footer />
+  //     </>
+  //   ),
+  // },
+  // {
+  //   path: "/gallery",
+  //   element: (
+  //     <>
+  //       <Navbar />
+  //       <Gallery />
+  //       <Footer />
+  //     </>
+  //   ),
+  // },
+  // {
+  //   path: "/about",
+  //   element: (
+  //     <>
+  //       <Navbar />
+  //       <About />
+  //       <Footer />
+  //     </>
+  //   ),
+  // },
+  // {
+  //   path: "/contact",
+  //   element: (
+  //     <>
+  //       <Navbar />
+  //       <Contact />
+  //       <Footer />
+  //     </>
+  //   ),
+  // },
+]);
+
+const App = () => {
+  return (
+    <>
+      <RouterProvider router={router} />
+      <ScrollToTop
+        color="white"
+        smooth
+        style={{
+          backgroundColor: "#EF4444",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      />
+    </>
+  );
+};
 
 export default App;
